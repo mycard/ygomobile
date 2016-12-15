e82061a315fde4d19d2c67234779a95e2712dcd8
qq_36544207 <905309599@qq.com>
2016-12-15 10:57:40 +0800

    添加背景，添加默认残局文件

--- mobile/src/main/java/cn/garymb/ygomobile/activities/LogoActivity.java
+++ mobile/src/main/java/cn/garymb/ygomobile/activities/LogoActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
@@ -19,6 +20,8 @@ public class LogoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_logo);
         ImageView image = (ImageView) findViewById(R.id.logo);
         AlphaAnimation anim = new AlphaAnimation(0.1f, 1.0f);
